4cb90fb8e3d759beb7603a21dcf1932388bf18cd
joyjoyhong <153259284+joyjoyhong@users.noreply.github.com>
2023-12-26 14:32:04 +0800

    split gensql and eval script

---
+++ gensql.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+BASE_URL="<Your base url copied from step 1>"
+PUBLIC_KEY="<Public key copied from step 1>"
+PRIVATE_KEY="<Private key copied from step 1>"
+
+
+docker run --rm -v ./result:/app/result -e BASE_URL=$BASE_URL -e PUBLIC_KEY=$PUBLIC_KEY -e PRIVATE_KEY=$PRIVATE_KEY spider_chat2query python parse_cases.py --json_file=./spider/test_data/dev.json
